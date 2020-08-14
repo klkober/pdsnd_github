ee0418b8364517356e9ee4dbe83baadf2852996b
Khai-Kober <khai.kober@gmail.com>
2020-08-14 15:54:19 -0700

    Add description to bikeshare_2.py

--- bikeshare-2/bikeshare_2.py
+++ bikeshare-2/bikeshare_2.py
@@ -1,3 +1,5 @@
+    #This is the bkeshare python code for my project that takes intfo from other .csv files and filters them via the users request
+
 import time
 import pandas as pd
 import numpy as np
